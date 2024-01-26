e5770952dad5ea2b728293affeeed52b1172fa4f
MyoungGeun <maijuana@naver.com>
2024-01-26 11:05:33 +0900

    화면 구성 중

--- src/main/java/com/tenco/bank/repository/entity/Account.java
+++ src/main/java/com/tenco/bank/repository/entity/Account.java
@@ -17,12 +17,21 @@ public class Account {
 	private Integer id;
 	private String number;
 	private String password;
-	private Long balance;
+	private Long balance; // 잔액
 	private Integer user_id;
 	private Timestamp created_at;
 	
 	// 출금 기능
+	public void withdraw(Long amount) {
+		// 방어적 코드 작성? - todo => - 금액, balance를 넘는 금액을 출금 할 경우를 막아야 함.
+		this.balance -= amount;
+	}
+	
 	// 입금 기능
+	public void deposit(Long amount) {
+		this.balance += amount;
+	}
+	
 	// 패스워드 체크
 	// 잔액 여부 확인 기능
 	// 계좌 소유자 확인 기능
